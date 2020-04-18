5242ade0d5db600d505957ca869ddb01747bb459
Paul Barthmaier <paul@barthmaier.rocks>
2020-04-18 11:52:09 -0400

    build assets

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 			{ name: "Ryu", age: 30, belt: "black", id: 1 },
 			{ name: "Yoshi", age: 20, belt: "green", id: 2 },
 			{ name: "Crystal", age: 25, belt: "pink", id: 3 },
+			{ name: "Toshi", age: 28, belt: "orange", id: 4 },
 		],
 	};
 	render() {
