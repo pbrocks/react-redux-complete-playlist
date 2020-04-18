18b7a2788d9b487fb66827ffecc6aee911fc7a6d
Paul Barthmaier <paul@barthmaier.rocks>
2020-04-18 12:50:04 -0400

    build assets

---
+++ myapp/src/lesson-info.js
@@ -0,0 +1,10 @@
+const jsonArray = {
+	"id": 15,
+	"title": "Stateless Components",
+	"url": "https://www.youtube.com/watch?v=o4GCcSn-hEw",
+	"description": "explains the difference between stateless / functional components and when we can use them instead of class based components.",
+	"comments": "Components have state, functions don't"
+};
+
+
+export default jsonArray;
