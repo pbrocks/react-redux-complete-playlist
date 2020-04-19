3dae4bd458a7eb8400849a7c09e683eae2a10b8b
Paul Barthmaier <paul@barthmaier.rocks>
2020-04-19 11:39:19 -0400

    build

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Ninjas from './Ninjas'
 import AddNinja from './AddNinja'
 import './App.css'
+import lessonInfo from './lesson-info';
 
 class App extends Component {
 	state = {
 		ninjas: [
 			{ name: 'Ryu', age: 30, belt: 'black', id: 1 },
 			{ name: 'Yoshi', age: 20, belt: 'green', id: 2 },
-			{ name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+			{ name: 'Crystal', age: 25, belt: 'pink', id: 3 },
+			{ name: "Toshi", age: 28, belt: "orange", id: 4 },
 		]
 	}
 	addNinja = (ninja) => {
@@ -30,9 +32,18 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>My first React app</h1>
+				<h1>{lessonInfo.title}</h1>
+				<p>{lessonInfo.point}</p>
 				<Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja} />
 				<AddNinja addNinja={this.addNinja} />
+				<p id="output">Output</p>
+				<div>
+					<h4>Lesson Info</h4>
+					<p><a href={lessonInfo.url} target="_blank">Lesson {lessonInfo.id}</a>&nbsp;
+					{lessonInfo.description}</p>
+					<h4>Lesson Comments</h4>
+					<p>{lessonInfo.comments}</p>
+				</div>
 			</div>
 		);
 	}
---
+++ myapp/src/lesson-info.js
@@ -0,0 +1,11 @@
+const jsonArray = {
+	"id": 21,
+	"title": "CSS Files",
+	"url": "https://www.youtube.com/watch?v=NFYJZyZIa70",
+	"point": "Add to the array of Ninjas",
+	"description": " shows you how you use and import CSS files into your components. For more info on CSS modules, see the link below.",
+	"comments": "https://blog.pusher.com/css-modules-react/ On click the resulting object from the form is output in console and on screen"
+};
+
+
+export default jsonArray;
