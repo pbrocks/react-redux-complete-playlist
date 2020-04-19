9ed4c1f0268ee4afe25a0ad246a369a4dc429bf4
Paul Barthmaier <paul@barthmaier.rocks>
2020-04-19 10:46:35 -0400

    lesson19

---
+++ myapp/src/lesson-info.js
@@ -0,0 +1,11 @@
+const jsonArray = {
+	"id": 19,
+	"title": "Deleting Data",
+	"url": "https://www.youtube.com/watch?v=UmuLW78biBw",
+	"point": "Add to the array of Ninjas",
+	"description": " shows how we can delete data from the state.",
+	"comments": "On click the resulting object from the form is output in console and on screen"
+};
+
+
+export default jsonArray;
