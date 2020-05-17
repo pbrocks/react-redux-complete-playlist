a89f32c59770d553464aff6bf0a7bfc073869981
Paul Barthmaier <paul@barthmaier.rocks>
2020-05-17 11:09:08 -0400

    lesson-29

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -3,21 +3,23 @@ import Navbar from './components/Navbar'
 import { Route, BrowserRouter } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
+import Harvey from './components/Harvey'
 import Contact from './components/Contact'
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Route exact path='/' component={Home}/>
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-        </div>
-      </BrowserRouter>
-    );
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<div className="App">
+					<Navbar />
+					<Route exact path='/' component={Home} />
+					<Route path='/about' component={About} />
+					<Route path='/harvey' component={Harvey} />
+					<Route path='/contact' component={Contact} />
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
---
+++ poketimes/src/components/Harvey.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Harvey = () => {
+	return (
+		<div>
+			<div className="container">
+				<h4 className="center">Harvey</h4>
+				<p>Chuck Norris can slam a revolving door. Chuck Norris can have his cake and eat it, too. Chuck Norris is currently suing NBC, claiming Law and Order are trademarked names for his left and right legs Chuck Norris can slice meat so thin is only has one side, When Chuck Norris sends in his taxes, he sends blank forms and includes only a picture of himself, crouched and ready to attack. Chuck Norris has not had to pay taxes, ever Chuck Norris is the only man to ever defeat a brick wall in a game of tennis. Chuck Norris looks gift horses in the mouth. Chuck Norris doesn’t wear a watch. HE decides what time it is, The Great Wall of China was originally created to keep Chuck Norris out. It failed miserably.</p>
+				<p>Chuck ipsum. Multiple people have died from Chuck Norris giving them the finger. Chuck Norris doesn’t need to swallow when eating food. Crop circles are Chuck Norris’ way of telling the world that sometimes corn needs to lie the f*ck down. Chuck Norris ordered a Big Mac at Burger King, and got one.</p>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae repudiandae repellat illo magni eligendi cupiditate voluptates eius nam voluptate. Incidunt nihil ullam quae quia officia quaerat, deserunt eligendi explicabo totam?</p>
+			</div>
+		</div>
+	)
+}
+
+export default Harvey
